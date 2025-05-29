a72e3297b9d871e43a07bf57fb60c2e0f38e8031
rohitmenonhart-xhunter <rohitmenongeneral@gmail.com>
2025-05-29 18:20:20 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  optimizeDeps: {
+    exclude: ['@react-three/postprocessing', '@amplitude/analytics-browser'],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
+  }
+}));
